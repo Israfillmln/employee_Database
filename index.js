c90e59017060b9565a776db1b1fce34a641bf65c
Israfillmln <israfilmaulana925@gmail.com>
2022-03-22 14:54:23 +0700

    review again

--- index.js
+++ index.js
@@ -1,50 +1,52 @@
-// Duplikasi method .find
-// Duplikasi method .indexOf
+// // Duplikasi method .find
+// // Duplikasi method .indexOf
 
-const fruits = ["Jeruk", "Apel", "Mangga", "Leci"]
+// const fruits = ["Jeruk", "Apel", "Mangga", "Leci"]
 
-// Conditions (indexOf)
-// 1. Mencari index dari sebuah item dalam array
-// 2. Akan return sebuah number (index)
+// // Conditions (indexOf)
+// // 1. Mencari index dari sebuah item dalam array
+// // 2. Akan return sebuah number (index)
 
-// console.log(fruits.indexOf("Leci"))
+// // console.log(fruits.indexOf("Leci"))
 
-// const duplikatIndexOf = (arr = [], dataToFind = "") => {
+// // const duplikatIndexOf = (arr = [], dataToFind = "") => {
+// //   for (let i = 0; i < arr.length; i++) {
+// //     if (arr[i] == dataToFind) {
+// //       return i
+// //     }
+// //   }
+
+// //   return -1
+// // }
+
+// // console.log(duplikatIndexOf(fruits, "Leci"))
+
+// // // Conditions (find)
+// // // 1. Mencari sebuah value (item) dalam array yang
+// // //    memenuhi condition yang kita berikan
+// // // 2. Callback function kita harus return boolean
+// // // 3. Ketika callback function return true, loop akan berhenti
+// // //    lalu function find akan return item dari array.
+
+// console.log(fruits.find((val, idx, arr) => {
+//   return true
+// }))
+
+// const duplikatFind = (arr, callback) => {
 //   for (let i = 0; i < arr.length; i++) {
-//     if (arr[i] == dataToFind) {
-//       return i
+//     if (callback(arr[i], i, arr)) {
+//       return arr[i]
 //     }
 //   }
-
-//   return -1
 // }
 
-// console.log(duplikatIndexOf(fruits, "Leci"))
-
-// // Conditions (find)
-// // 1. Mencari sebuah value (item) dalam array yang
-// //    memenuhi condition yang kita berikan
-// // 2. Callback function kita harus return boolean
-// // 3. Ketika callback function return true, loop akan berhenti
-// //    lalu function find akan return item dari array.
-
-console.log(fruits.find((val, idx, arr) => {
-  return true
-}))
-
-const duplikatFind = (arr, callback) => {
-  for (let i = 0; i < arr.length; i++) {
-    if (callback(arr[i], i, arr)) {
-      return arr[i]
-    }
-  }
-}
-
-console.log(duplikatFind(fruits, (val) => {
-  return val.length > 5
-}))
+// console.log(duplikatFind(fruits, (val) => {
+//   return val.length > 5
+// }))
 
 // const ages = [13, 7, 1, 3, 2, 8, 3, 26]
+
+// console.log(ages.sort((a, b) => a - b))
 
 // // const copyFruits = [ ...fruits ]
 // // copyFruits.sort()
@@ -58,36 +60,36 @@ console.log(duplikatFind(fruits, (val) => {
 
 // // console.log(ages)
 
-// const people = [
-//   {
-//     fullName: "seto",
-//     age : 17
-//   },
-//   {
-//     fullName: "sasuke",
-//     age : 29
-//   },
-//   {
-//     fullName: "doraemon",
-//     age : 13
-//   },
-//   {
-//     fullName: "bill",
-//     age : 40
-//   },
-// ]
+const people = [
+  {
+    fullName: "seto",
+    age : 17
+  },
+  {
+    fullName: "sasuke",
+    age : 29
+  },
+  {
+    fullName: "doraemon",
+    age : 13
+  },
+  {
+    fullName: "bill",
+    age : 40
+  },
+]
 
-// people.sort((a, b) => {
-//   if (a.age > b.age) {
-//     return 1
-//   }
+console.log(people.sort((a, b) => {
+  if (a.fullName > b.fullName) {
+    return 1
+  }
 
-//   if (b.age > a.age) {
-//     return -1
-//   }
+  if (b.fullName > a.fullName) {
+    return -1
+  }
 
-//   return 0
-// })
+  return 0
+}))
 
 // const seto = {
 //   fullName: "seto",
